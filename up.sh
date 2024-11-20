5f05e0ed055080fdfd46903a5d621caee98e2659
arunacharya528 <arunacharya255@gmail.com>
2024-11-20 15:59:18 +0545

    Create up.sh

---
+++ up.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+
+echo "$($PSQL "
+CREATE TABLE teams(
+  team_id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) UNIQUE NOT NULL
+);
+")"
+
+echo "$($PSQL "
+CREATE TABLE games(
+  game_id SERIAL PRIMARY KEY NOT NULL,
+  year INTEGER NOT NULL,
+  round VARCHAR(255) NOT NULL,
+  winner_id INTEGER REFERENCES teams(team_id) NOT NULL,
+  opponent_id INTEGER REFERENCES teams(team_id) NOT NULL,
+  winner_goals INTEGER NOT NULL,
+  opponent_goals INTEGER NOT NULL
+);
+")"
